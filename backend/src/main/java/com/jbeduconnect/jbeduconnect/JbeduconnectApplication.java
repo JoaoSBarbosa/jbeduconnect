b9f589932285f9c3beb57bea619837b217a29cfd
João Barbosa <contato.jsbarbosa@gmail.com>
2023-12-10 17:11:00 -0300

    Starting the project

---
+++ backend/src/main/java/com/jbeduconnect/jbeduconnect/JbeduconnectApplication.java
@@ -0,0 +1,13 @@
+package com.jbeduconnect.jbeduconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JbeduconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JbeduconnectApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/jbeduconnect/jbeduconnect/JbeduconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jbeduconnect.jbeduconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JbeduconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
