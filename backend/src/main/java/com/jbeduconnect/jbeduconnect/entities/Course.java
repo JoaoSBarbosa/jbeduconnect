ad6dbb948773d517a19aff74eb6f8e843228dc8e
João Barbosa <contato.jsbarbosa@gmail.com>
2023-12-19 20:16:49 -0300

    Entities-> course and offers

--- backend/src/main/java/com/jbeduconnect/jbeduconnect/entities/Course.java
+++ backend/src/main/java/com/jbeduconnect/jbeduconnect/entities/Course.java
@@ -3,6 +3,7 @@ package com.jbeduconnect.jbeduconnect.entities;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
@@ -17,17 +18,59 @@ public class Course implements Serializable {
     private String imgGrayUri;
 
     @OneToMany
-    private List<Offer> offers;
+    private List<Offer> offers = new ArrayList<>();
 
-    private Course(){}
+    private Course(){
 
-    public Course(Long id, String name, String imgUri, String imgGrayUri) {
+    }
+
+    public Course(Long id, String name, String imgUri, String imgGrayUri, List<Offer> offers) {
         this.id = id;
         this.name = name;
         this.imgUri = imgUri;
         this.imgGrayUri = imgGrayUri;
+        this.offers = offers;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImgUri() {
+        return imgUri;
+    }
+
+    public void setImgUri(String imgUri) {
+        this.imgUri = imgUri;
+    }
+
+    public String getImgGrayUri() {
+        return imgGrayUri;
+    }
+
+    public void setImgGrayUri(String imgGrayUri) {
+        this.imgGrayUri = imgGrayUri;
+    }
+
+    public List<Offer> getOffers() {
+        return offers;
+    }
+
+    public void setOffers(List<Offer> offers) {
+        this.offers = offers;
+    }
 
     @Override
     public boolean equals(Object o) {
--- backend/src/main/java/com/jbeduconnect/jbeduconnect/entities/Offer.java
+++ backend/src/main/java/com/jbeduconnect/jbeduconnect/entities/Offer.java
@@ -4,9 +4,10 @@ import jakarta.persistence.*;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.Objects;
 
 @Entity
-@Table(name = "tb_offers")
+@Table(name = "tb_offer")
 public class Offer implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,6 +17,69 @@ public class Offer implements Serializable {
     private Instant endMoment;
 
     @ManyToOne
-    @JoinColumn
+    @JoinColumn(name = "course_id")
     private Course course;
+
+    public Offer(){}
+
+    public Offer(Long id, String edition, Instant startMoment, Instant endMoment, Course course) {
+        this.id = id;
+        this.edition = edition;
+        this.startMoment = startMoment;
+        this.endMoment = endMoment;
+        this.course = course;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEdition() {
+        return edition;
+    }
+
+    public void setEdition(String edition) {
+        this.edition = edition;
+    }
+
+    public Instant getStartMoment() {
+        return startMoment;
+    }
+
+    public void setStartMoment(Instant startMoment) {
+        this.startMoment = startMoment;
+    }
+
+    public Instant getEndMoment() {
+        return endMoment;
+    }
+
+    public void setEndMoment(Instant endMoment) {
+        this.endMoment = endMoment;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Offer offer = (Offer) o;
+        return Objects.equals(id, offer.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
